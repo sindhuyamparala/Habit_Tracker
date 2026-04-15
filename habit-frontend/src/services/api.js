a0a0fe04ff497936c676a28a8c55ec449d38461a
sindhuyamparala <144418440+sindhuyamparala@users.noreply.github.com>
2026-04-15 12:18:36 +0530

    Update api.js

--- habit-frontend/src/services/api.js
+++ habit-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://habit-tracker-1-oww7.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
